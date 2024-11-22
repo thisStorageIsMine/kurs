094a1562455ef0293c60900bf835869eb7fdcb86
thisStorageIsMine <bezrukovvaler@yandex.ru>
2024-11-22 23:30:15 +0300

    feat:Add  metadata file

---
+++ metadata.ts
@@ -0,0 +1,17 @@
+export const meta = {
+    default: {
+        title: 'Заметочкая',
+        description: "Твой выбор для быстрых записей",
+        keywords: "блокнот, блокнот онлайн, заметки, записи, notes, notes online"
+    },
+    login: {
+        title: 'Войти в заметочную',
+        description: "Добро пожаловать домой",
+        keywords: "вход, login, войти в аккаунт"
+    },
+    signup: {
+        title: 'Создать аккаунт',
+        description: "Привет! Хочешь создать аккаунт?",
+        keywords: "signup, зарегистрироваться"
+    },
+}
